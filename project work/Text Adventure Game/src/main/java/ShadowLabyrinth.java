cf3b14400c7e2e954dedbd841a152ed50a6de997
pepsilad2222 <118309780+pepsilad2222@users.noreply.github.com>
2024-11-28 20:44:34 -0600

    Update ShadowLabyrinth.java

--- project work/Text Adventure Game/src/main/java/ShadowLabyrinth.java
+++ project work/Text Adventure Game/src/main/java/ShadowLabyrinth.java
@@ -42,7 +42,7 @@ public class ShadowLabyrinth {
         typeTextWithCursor("Much to your suprise you look around to find nothing",50);
         typeTextWithCursor("You are surrounded by black expect a small room heading forward...",50);
         typeTextWithCursor("As curiosity entices you you step forward....",50);
-        typeTextWithCursor("As you step in you remeber your love getting captured...",50);
+        typeTextWithCursor("As you step in you remember your love getting captured...",50);
         typeTextWithCursor("You run and run and run until you find an enemy",50);
         typeTextWithCursor("With useless destroyed armor and a damaged sword you look around..",50);
         typeTextWithCursor("You then charge forth, attempting to save your love",50);
